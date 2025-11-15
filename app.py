e41047b7085c2a831dd555bc56440f19e33d66aa
Evinedim <nikvoron3008@gmail.com>
2025-11-15 16:13:12 +0300

    Add method to draw graph

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import tkinter as tk 
 from tkinter import ttk
+import math
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
 
 class App(tk.Tk):
     def __init__(self):
@@ -10,7 +20,7 @@ class App(tk.Tk):
 
         self.create_widgets()
         self.setup_layout()
-        self.draw_graph()
+        self.draw_graph(graph)
 
         self.mainloop()
 
@@ -20,8 +30,26 @@ class App(tk.Tk):
     def setup_layout(self):
         self.canvas.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
-    def draw_graph(self):
-        pass
+    def draw_graph(self, graph):
+        vertexes = set(graph.keys())
+        edges = set((n, v) for n in graph for v in graph[n])
+
+        coords = {}
+        for i, vertex in enumerate(vertexes):
+            angle = 2 * math.pi * (i / len(vertexes))
+
+            coords[vertex] = (
+                300 + 150 * math.cos(angle), 
+                300 + 150 * math.sin(angle)
+            )
+
+        for v1, v2 in edges:
+            self.canvas.create_line(*coords[v1], *coords[v2], width=3, fill='black')
+
+        for vertex, (x, y) in coords.items():
+            self.canvas.create_oval(x - 20, y - 20, x + 20, y + 20, fill='lightgreen', outline='black', width=3)
+            self.canvas.create_text(x, y, text=str(vertex), font=('Arial', 12))
+            
 
 
 app = App()
